98b16057a57f2a90d57703cea46507bdbfb089b6
jowynd <jowynd12@gmail.com>
2024-06-27 22:03:36 -0300

    Post - findByTitle

--- src/main/java/com/test/workshopmongo/resources/PostResources.java
+++ src/main/java/com/test/workshopmongo/resources/PostResources.java
@@ -2,20 +2,34 @@ package com.test.workshopmongo.resources;
 
 import com.test.workshopmongo.entities.Post;
 import com.test.workshopmongo.repositorys.PostRepository;
+import com.test.workshopmongo.resources.util.URL;
 import com.test.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/posts")
 public class PostResources {
+    @Autowired
     private PostService service;
 
-    @GetMapping(name = "/{id}")
+    @GetMapping(value = "/{id}")
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post post = service.findById(id);
 
         return ResponseEntity.ok().body(post);
+    }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findBytitle(@RequestParam(value = "text", defaultValue = "") String text) {
+
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+
+        return ResponseEntity.ok().body(list);
     }
 
 }
--- src/main/java/com/test/workshopmongo/services/PostService.java
+++ src/main/java/com/test/workshopmongo/services/PostService.java
@@ -4,9 +4,12 @@ import com.test.workshopmongo.entities.Post;
 import com.test.workshopmongo.repositorys.PostRepository;
 import com.test.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
+@Service
 public class PostService {
 
     @Autowired
@@ -16,5 +19,9 @@ public class PostService {
         Optional<Post> post = repository.findById(id);
 
         return post.orElseThrow(() -> new ObjectNotFoundException("Not found."));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return repository.findByTitleContainingIgnoreCase(text);
     }
 }
