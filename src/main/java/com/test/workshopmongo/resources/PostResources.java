66e46d1469cd8baa26b5f70741395ca542fc0757
jowynd <jowynd12@gmail.com>
2024-06-29 00:16:26 -0300

    query with multiple criteria

--- src/main/java/com/test/workshopmongo/resources/PostResources.java
+++ src/main/java/com/test/workshopmongo/resources/PostResources.java
@@ -1,13 +1,13 @@
 package com.test.workshopmongo.resources;
 
 import com.test.workshopmongo.entities.Post;
-import com.test.workshopmongo.repositorys.PostRepository;
 import com.test.workshopmongo.resources.util.URL;
 import com.test.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -32,4 +32,15 @@ public class PostResources {
         return ResponseEntity.ok().body(list);
     }
 
+    @GetMapping(value = "/fullsearch")
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value="text", defaultValue="") String text,
+            @RequestParam(value="minDate", defaultValue="") String minDate,
+            @RequestParam(value="maxDate", defaultValue="") String maxDate) {
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+        List<Post> list = service.fullSearch(text, min, max);
+        return ResponseEntity.ok().body(list);
+    }
 }
